bde373ef298153f9b98c9781c2fb3f2e5d509131
Krista Singleton <krista.singleton@intimetec.com>
2021-03-26 21:47:17 -0700

    Installed redux-thunk

---
+++ client/src/components/DCandidateForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DCandidateForm = () => {
+  console.log('DCanddate form component fired.');
+  return (
+    <div>DCandidateForm</div>
+  );
+};
+
+export default DCandidateForm;
---
+++ client/src/components/DCandidates.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DCandidates = () => {
+  console.log('DCandidates component fired.');
+  return (
+    <div>DCandidates</div>
+  );
+};
+
+export default DCandidates;
